2178b17fbeeb28d46a9a1c01c1d641bbdedf2f3d
Vivisector <117979576+Vivisector@users.noreply.github.com>
2023-04-09 00:35:21 +0300

    Create Main.java

---
+++ Seminar4/Main.java
@@ -0,0 +1,19 @@
+// package Seminar4.tasks;
+
+// import tasks.task0;
+// import Seminar.Task4;
+import tasks.Dz1revers;
+// import tasks.Task1;
+// import tasks.Task3;
+// import tasks.mStack;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // task0.run();
+        // Task3.main(args);
+        int[] arr = {1,2,5,4,6,7,8,45,67};
+        Dz1revers.main(arr);
+        Dz1revers.revers(arr);
+    }
+}
